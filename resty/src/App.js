d73ffe61d64ec298769fb8c5d518ad81b47bbfde
samah anwer alsatary <alsatary.samah@yahoo.com>
2022-08-10 11:07:14 +0300

    css file

--- resty/src/App.js
+++ resty/src/App.js
@@ -5,10 +5,10 @@ import './scss/app.scss';
 
 // Let's talk about using index.js and some other name in the component folder
 // There's pros and cons for each way of doing this ...
-import Header from './components/header';
-import Footer from './components/footer';
-import Form from './components/form';
-import Results from './components/results';
+import Header from './components/header/header';
+import Footer from './components/footer/footer';
+import Form from './components/form/form';
+import Results from './components/result/results';
 // import callApi from "./hooks/callApi";
 
 function App (){
